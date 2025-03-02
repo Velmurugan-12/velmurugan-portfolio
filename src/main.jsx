816f63cc9d42f2d1741aded5154941bfc326e74e
Velmurugan-12 <mvelmurugan2192@gmail.com>
2025-03-02 20:38:53 +0530

    main file is updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import ytClone from "/assets/yt-clone.mp4";
-import loginform from "/assets/loginform.mp4";
+
 
 export const MyContext = React.createContext();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MyContext.Provider value={{ 
-      ytClone,
-      loginform,
+      ytClone: "/yt-clone.mp4",   
+      loginform: "/loginform.mp4"
     }}>
       <App />
     </MyContext.Provider>
