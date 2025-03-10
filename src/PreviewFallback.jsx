ae21fc7b600869e1aaece65e8d22e0e9b694bd2b
Velmurugan-12 <mvelmurugan2192@gmail.com>
2025-03-10 13:26:45 +0530

    Updates

--- src/PreviewFallback.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PreviewFallback = () => {
-  return (
-    <div className='group relative rounded-xl grid-rows-2 overflow-hidden bg-gray-700 animate-pulse h-[300px]'></div>
-  );
-};
-
-export default PreviewFallback;
